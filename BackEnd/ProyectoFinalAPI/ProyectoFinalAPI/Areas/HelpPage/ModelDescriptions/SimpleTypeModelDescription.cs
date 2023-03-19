d02edf14fe38137f6b69a60319e446f841107e24
Luis Esquivel <luis.jose.esquivel@gmail.com>
2023-03-19 10:51:45 -0600

    added frontend and backend

---
+++ BackEnd/ProyectoFinalAPI/ProyectoFinalAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProyectoFinalAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
--- Lesson1/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lesson1.MvcApplication" Language="C#" %>
