50ba87785002dbba0b67c02f83cda20f763aaa04
Luis Esquivel <luis.jose.esquivel@gmail.com>
2023-03-23 21:37:54 -0600

    carrusel terminado con API de perros y gatos

---
+++ BackEnd/ProyectoFinalAPI/ProyectoFinalAPI/Servicios/GatoService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Web;
+using System.Net.Http.Json;
+using System.Text.Json;
+using ProyectoFinalAPI.Entities;
+
+namespace ProyectoFinalAPI.Servicios
+{
+    public class GatoService
+    {
+        public string RandomCat()
+        {
+
+            using (var client = new HttpClient())
+            {
+                string url = "https://cataas.com/cat?json=true";
+
+                //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Current.Session["Token"].ToString());
+                HttpResponseMessage respuesta = client.GetAsync(url).GetAwaiter().GetResult();
+
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    string jsonResponse = respuesta.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    var data = JsonSerializer.Deserialize<CatsFotoEnt>(jsonResponse);
+                    return String.Format("https://cataas.com{0}", data.url);
+                }
+
+                return "No response";
+            }
+        }
+    }
+}
