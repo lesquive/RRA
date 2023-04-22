1cbb155451e14e44479ab5ac9887bc0eaf151294
Luis Esquivel <luis.jose.esquivel@gmail.com>
2023-04-22 00:28:58 -0600

    función de adopciones añadida

---
+++ BackEnd/ProyectoFinalAPI/ProyectoFinalAPI/Entities/AdoptarEnt.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalAPI.Entities
+{
+    public class AdoptarEnt
+    {
+        public long id { get; set; }
+
+        public int animal_ID { get; set; }
+
+        public int refugio_ID { get; set; }
+
+        public int adoptante_ID { get; set; }
+
+        public DateTime fecha { get; set; }
+    }
+}
